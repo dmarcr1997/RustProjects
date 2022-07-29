0c1e4261e9571e3eeb742b55688769120917dc91
Damon Rocha <dmarcr1997@gmail.com>
2022-07-29 14:27:07 -0400

    RustyPipeViewer - implemented mpsc channel connection

--- RustyPipeViewer/src/args.rs
+++ RustyPipeViewer/src/args.rs
@@ -4,27 +4,25 @@ use std::env;
 pub struct Args {
     pub infile: String,
     pub outfile: String,
-    pub silent: bool
+    pub silent: bool,
 }
 
 impl Args {
     pub fn parse() -> Self {
         let matches = App::new("pipeviewer")
-        .arg(Arg::with_name("infile").help("Read from a file instead of stdin"))
-        .arg(Arg::with_name("outfile")
-            .short("o")
-            .long("outfile")
-            .takes_value(true)
-            .help("Write output to a file instead of stdout")
-        )
-        .arg(Arg::with_name("silent")
-            .short("s")
-            .long("silent")
-        )
-        .get_matches();
+            .arg(Arg::with_name("infile").help("Read from a file instead of stdin"))
+            .arg(
+                Arg::with_name("outfile")
+                    .short("o")
+                    .long("outfile")
+                    .takes_value(true)
+                    .help("Write output to a file instead of stdout"),
+            )
+            .arg(Arg::with_name("silent").short("s").long("silent"))
+            .get_matches();
         let infile = matches.value_of("infile").unwrap_or_default().to_string();
         let outfile = matches.value_of("outfile").unwrap_or_default().to_string();
-        let silent = if matches.is_present("silent"){
+        let silent = if matches.is_present("silent") {
             true
         } else {
             !env::var("PV_SILENT").unwrap_or_default().is_empty()
@@ -33,7 +31,7 @@ impl Args {
         Self {
             infile,
             outfile,
-            silent
+            silent,
         }
     }
 }
